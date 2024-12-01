d93c839c0a5933fa18d46916e9d0d3c76285d957
Bre4dGC <glebbreadc@gmail.com>
2024-12-02 00:45:41 +0300

    Created Auth and Regist Pages

---
+++ PassGuard/Views/Pages/Login/AuthenticationPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using PassGuard.ViewModels.Login;
+using System.Windows.Controls;
+
+namespace PassGuard.Views.Pages.Login
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthenticationPage.xaml
+    /// </summary>
+    public partial class AuthenticationPage : Page
+    {
+        public AuthenticationPage()
+        {
+            InitializeComponent();
+
+            DataContext = new AuthenticationViewModel(NavigationService);
+        }
+    }
+}
---
+++ PassGuard/Views/Pages/Login/RegistrationPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using PassGuard.ViewModels.Login;
+using System.Windows.Controls;
+
+namespace PassGuard.Views.Pages.Login
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+
+            DataContext = new RegistrationViewModel(NavigationService);
+        }
+    }
+}
