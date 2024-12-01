558783e2929cb752f6d90f9c3bff813a4d67ded4
Bre4dGC <glebbreadc@gmail.com>
2024-12-02 00:44:30 +0300

    Created DataInputField and SubmitButton Controls

---
+++ PassGuard/Styles/Components/DataInputField.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PassGuard.Styles.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для DataInputField.xaml
+    /// </summary>
+    public partial class DataInputField : UserControl
+    {
+        public DataInputField()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PassGuard/Styles/Components/SubmitButton.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PassGuard.Styles.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для SubmitButton.xaml
+    /// </summary>
+    public partial class SubmitButton : UserControl
+    {
+        public SubmitButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(SubmitButton));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(ButtonCommandProperty);
+            set => SetValue(ButtonCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty ButtonCommandProperty =
+        DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(SubmitButton), new PropertyMetadata(null));
+    }
+}
