c1fdbfa235a6844025a7d3af9b28dcceee874de7
Bre4dGC <glebbreadc@gmail.com>
2024-12-02 00:44:58 +0300

    Created Login ViewModels

---
+++ PassGuard/ViewModels/Login/AuthenticationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using PassGuard.Views.Pages.Login;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace PassGuard.ViewModels.Login
+{
+    class AuthenticationViewModel
+    {
+        public ICommand RegistrationViewCommand { get; }
+        public ICommand AuthenticationCommand { get; }
+
+        public AuthenticationViewModel(NavigationService navigationService)
+        {
+            AuthenticationCommand = new RelayCommand(Authentication);
+            RegistrationViewCommand = new RelayCommand(() => navigationService.Navigate(new RegistrationPage()));
+        }
+
+        private void Authentication()
+        {
+
+        }
+    }
+}
---
+++ PassGuard/ViewModels/Login/RegistrationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using PassGuard.Views.Pages.Login;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace PassGuard.ViewModels.Login
+{
+    class RegistrationViewModel
+    {
+        public ICommand AuthenticationViewCommand { get; }
+
+        public RegistrationViewModel(NavigationService navigationService)
+        {
+            AuthenticationViewCommand = new RelayCommand(() => navigationService.Navigate(new AuthenticationPage()));
+        }
+    }
+}
