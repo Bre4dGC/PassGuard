047566d92154cfd2799b07afc7359906412b0a02
Bre4dGC <glebbreadc@gmail.com>
2024-12-02 00:44:02 +0300

    Created DbContexts

---
+++ PassGuard/Data/DbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PassGuard.Data
+{
+    class DbContextFactory : IDesignTimeDbContextFactory<PassGuardDbContext>
+    {
+        public PassGuardDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<PassGuardDbContext>();
+            options.UseSqlite("");
+
+            return new PassGuardDbContext(options.Options);
+        }
+    }
+}
---
+++ PassGuard/Data/PassGuardDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassGuard.Models;
+
+namespace PassGuard.Data
+{
+    class PassGuardDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public PassGuardDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(c => c.Services)
+                .WithOne(m => m.User)
+                .HasForeignKey(m => m.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
