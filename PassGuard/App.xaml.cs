d48639be88fd061eddb13af59b2723c5ee3611db
Bre4dGC <glebbreadc@gmail.com>
2024-12-02 00:46:13 +0300

    Init Project

---
+++ PassGuard/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PassGuard.Views;
+using PassGuard.Views.Windows;
+using System.Windows;
+
+namespace PassGuard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+    }
+
+}
