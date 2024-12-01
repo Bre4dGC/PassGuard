dd97a76c5d6eeffe4ca8eaa8d2927a2739ee6005
Bre4dGC <glebbreadc@gmail.com>
2024-12-02 00:45:57 +0300

    Created Login and Main Windows

---
+++ PassGuard/Views/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using PassGuard.Views.Pages.Login;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PassGuard.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+
+            LoginFrame.Content = new AuthenticationPage();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+
+            if(e.ClickCount == 2)
+            {
+                WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ PassGuard/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Dark.Net;
+using System.Drawing;
+using System.Windows;
+
+namespace PassGuard.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DarkNet.Instance.SetWindowThemeWpf(this, Theme.Dark, new ThemeOptions
+            {
+                TitleBarBackgroundColor = Color.FromArgb(25, 38, 52)
+            });
+
+            DarkNet.Instance.SetWindowThemeWpf(this, Theme.Auto);
+        }
+    }
+}
