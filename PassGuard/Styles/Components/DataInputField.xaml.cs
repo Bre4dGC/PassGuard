f2ea5d32176619e55d7671770e33e5d55d312991
Bre4dGC <glebbreadc@gmail.com>
2024-12-03 22:21:51 +0300

    Updated Components

--- PassGuard/Styles/Components/DataInputField.xaml.cs
+++ PassGuard/Styles/Components/DataInputField.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace PassGuard.Styles.Components
 {
@@ -10,6 +11,28 @@ namespace PassGuard.Styles.Components
         public DataInputField()
         {
             InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(DataInputField), new PropertyMetadata(string.Empty, OnTextChanged));
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DataInputField control)
+            {
+                control.OnTextChanged(e);
+            }
+        }
+
+        private void OnTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            TextBox.Text = Text;
         }
     }
 }
