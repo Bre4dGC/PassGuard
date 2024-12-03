965ecec2711722057758ea6cda147a3f64222f3e
Bre4dGC <glebbreadc@gmail.com>
2024-12-03 22:24:11 +0300

    Created Registration and Saving to DB

--- PassGuard/ViewModels/Login/RegistrationViewModel.cs
+++ PassGuard/ViewModels/Login/RegistrationViewModel.cs
@@ -1,17 +1,59 @@
 ﻿using CommunityToolkit.Mvvm.Input;
-using PassGuard.Views.Pages.Login;
+using PassGuard.Data;
+using PassGuard.Models;
+using PassGuard.Services;
 using System.Windows.Input;
-using System.Windows.Navigation;
 
 namespace PassGuard.ViewModels.Login
 {
-    class RegistrationViewModel
+    class RegistrationViewModel : BaseViewModel
     {
-        public ICommand AuthenticationViewCommand { get; }
-
-        public RegistrationViewModel(NavigationService navigationService)
+        private string _name;
+        public string Name
         {
-            AuthenticationViewCommand = new RelayCommand(() => navigationService.Navigate(new AuthenticationPage()));
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get { return _confirmPassword; }
+            set { _confirmPassword = value; OnPropertyChanged(); }
+        }
+
+        private ProtectionLevel _protectionLevel;
+        public ProtectionLevel ProtectionLevel
+        {
+            get { return _protectionLevel; }
+            set { _protectionLevel = value; OnPropertyChanged(); }
+        }
+
+        private IUserService _userService = new UserService(new DbContextFactory());
+
+        public ICommand CreateUserCommand { get; }
+
+        public RegistrationViewModel()
+        {
+            CreateUserCommand = new RelayCommand(Register);
+        }
+
+        private async void Register()
+        {
+            if (!string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Password) && ConfirmPassword == Password)
+            {
+                await _userService.Register(
+                    Name, 
+                    Password,
+                    ProtectionLevel);
+            }
         }
     }
 }
