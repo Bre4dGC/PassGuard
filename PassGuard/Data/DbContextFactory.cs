b8fc9f2a022ed574a8766860bef77cf640822839
Bre4dGC <glebbreadc@gmail.com>
2024-12-03 22:19:42 +0300

    Transition from SQLite to MS SQL

--- PassGuard/Data/DbContextFactory.cs
+++ PassGuard/Data/DbContextFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using System.IO;
 
 namespace PassGuard.Data
 {
@@ -8,7 +9,7 @@ namespace PassGuard.Data
         public PassGuardDbContext CreateDbContext(string[] args = null)
         {
             var options = new DbContextOptionsBuilder<PassGuardDbContext>();
-            options.UseSqlite("");
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PassGuardDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
             return new PassGuardDbContext(options.Options);
         }
