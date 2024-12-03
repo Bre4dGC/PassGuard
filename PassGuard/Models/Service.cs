f144235173b7a3c0f24cc2794be09ffb2ee8c704
Bre4dGC <glebbreadc@gmail.com>
2024-12-03 22:20:49 +0300

    Update Models

--- PassGuard/Models/Service.cs
+++ PassGuard/Models/Service.cs
@@ -2,18 +2,11 @@
 {
     public class Service : DomainObject
     {
-        public Guid UserId { get; }
+        public Guid UserId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public DateTime CreatedDate { get; }
+        public DateTime CreatedDate { get; set; }
         public ProtectionLevel ProtectionLevel { get; set; }
         public virtual User User { get; set; }
-    }
-
-    public enum ProtectionLevel
-    {
-        Weak, 
-        Normal, 
-        Strong
     }
 }
--- PassGuard/Models/User.cs
+++ PassGuard/Models/User.cs
@@ -3,8 +3,9 @@
     public class User : DomainObject
     {
         public string MasterPassword { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public ProtectionLevel ProtectionLevel { get; set; }
         public ICollection<Service> Services { get; set; } = new List<Service>();
     }
 }
