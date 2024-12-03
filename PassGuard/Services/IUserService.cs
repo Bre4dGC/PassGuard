1321bd91a31ec91547feece86d720c77960b88d5
Bre4dGC <glebbreadc@gmail.com>
2024-12-03 22:24:55 +0300

    Updated

--- PassGuard/Services/IUserService.cs
+++ PassGuard/Services/IUserService.cs
@@ -5,7 +5,7 @@ namespace PassGuard.Services
     internal interface IUserService
     {
         Task<bool> Login(string name, string password);
-        Task<User> Register(string name, string password, string confirmPassword, ProtectionLevel protectionLevel);
+        Task<User> Register(string name, string password, ProtectionLevel protectionLevel);
         Task<User> Update(Guid id, User user);
         Task<bool> Delete(Guid id);
     }
